ad82654285100b2443437bd9b1c856693ceafb62
soheeparklee <soheeparklee@gmail.com>
2023-11-21 23:12:28 +0900

    update: css animation, js keyboard

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let interval
 const answer= "PARIS"
 
 function appStart(){
+    const showAnimation= () =>{
+        const rowBox= document.querySelector(`.box_row[data-row= "${attempts-1}"]`)
+        rowBox.classList.add("animationEffect");
+        console.log(rowBox)
+
+    }
+
     const startTime= new Date();
     const timer= () =>{
         const timer= document.querySelector(".time");
@@ -33,6 +40,7 @@ function appStart(){
         failDiv.style= "display:flex; justify-content:center; align-items:center; position:absolute; top:50vh; width:50vw;"
     }
     const gameWin= () =>{
+        confetti();
         const winDiv = document.createElement("div");
         document.body.appendChild(winDiv);
         winDiv.innerText= "You Won";
@@ -58,28 +66,65 @@ function appStart(){
             //워들의 정답과 client의 정답이 일치하는지 한 글자씩 확인해야 함. 
             //client가 입력한 답 한 글자씩 가져오는 방법
             const thisBox= document.querySelector(`.box_block[data-index= "${attempts}${i}"]`)
-            const clientAnswer= thisBox.innerText;
+            const clientAnswer= thisBox.innerText
+            //keyboard의 색깔 바꾸기
+            const dataKey= thisBox.innerHTML;
+            console.log(dataKey)
+            const alphabetBox= document.querySelector(`.alphabet[data-key="${dataKey}"]`)
+            
             
             if (answer[i] === clientAnswer){
                 thisBox.style.background= "rgb(106,169,100)";
+                alphabetBox.style.background= "rgb(106,169,100)";
                 rightAnswer += 1;
                 console.log(rightAnswer);
             }else if (answer.includes(clientAnswer) ){
                 thisBox.style.background= "rgb(201,180,88)";
+                alphabetBox.style.background= "rgb(201,180,88)";
+                
             } else{
                 thisBox.style.background= "rgb(120,124,126)";
+                alphabetBox.style.background= "rgb(120,124,126)";
             }
 
             thisBox.style.color= "white";
+            alphabetBox.style.color= "white";
+            
 
         
         }
         if (rightAnswer === 5) {
             gameWin(); gameOver();
-        } else nextLine();
+        } else nextLine(); showAnimation();
         };
         
-    
+    //keyboard 클릭되는함수
+    const handleKeyboard= (event) =>{
+        const key= event.target.dataset.key
+        const keyCode= key.charCodeAt();
+
+        const thisBox= document.querySelector(`.box_block[data-index= "${attempts}${index}"]`)
+        if(index === 5 ){
+            if (key === "Enter") {
+                handleEnter();
+            } else if(key === "Delete") {
+                handleDelete();
+            } else return;
+        } 
+        else if(key === "Delete") {
+            handleDelete();
+        } 
+        else if(69 === keyCode){
+            return;
+        }
+        else if (65<= keyCode && keyCode <= 90){
+            thisBox.innerHTML= key;
+            index = index +1;
+            return key;
+            }
+    }
+
+    window.addEventListener("click", handleKeyboard)
 
     const handleKeyDown= (event) =>{
         
@@ -89,14 +134,16 @@ function appStart(){
         const thisBox= document.querySelector(`.box_block[data-index= "${attempts}${index}"]`);
         
         if(index === 5 ){
-            if (event.key === "Enter") {
+            if (keyCode === 13) {
                 handleEnter();
-            } else if(event.key === "Backspace") {
+            } else if(keyCode === 8) {
                 handleDelete();
             } else return;
         } else if (65<= keyCode && keyCode <= 90){
             thisBox.innerHTML= key;
             index = index +1;
+            return key;
+
         } 
         else if(event.key === "Backspace") {
             handleDelete();
